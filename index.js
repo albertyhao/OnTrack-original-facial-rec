3db94e854cec02f1eadfbcc9183d4f6e1c0b8e34
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2018-11-14 21:49:33 -0800

    f

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
 var express = require('express')
 	, path = require('path')
-	, mongoose = require('mongoose')
 	, bodyParser = require('body-parser')
 	, http = require('http')
-	, crypto = require('crypto')
-	, passport = require('passport')
-	, LocalStrategy = require('passport-local')
-	, flash = require('express-flash')
-	, session = require('express-session')
-	, cookieParser = require('cookie-parser')
 	, async = require('async')
-	, MongoDBStore = require('connect-mongodb-session')
 	, fs = require('fs')
 ;
 
-var server = http.createServer(app)
 var app = express()
-	, dbUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1/ontrack'
 	, port = parseInt(process.env.PORT || '8080')
-	, iterations = 10000
+	, server = http.createServer(app)
 ;
 
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/home', (req, res, next) => {
+	console.log('home')
 	var filePath = path.join(__dirname, './home.html')
 	res.sendFile(filePath);
 });
 
-console.log('Listening to port');
-	server.on('listening', () => {
-		console.log('testing');
-		var addr = server.address()
-			, bind = typeof addr === 'string'
-				? 'pipe ' + addr
-				: 'port ' + addr.port
-		;
+server.on('listening', () => {
+	var addr = server.address()
+		, bind = typeof addr === 'string'
+			? 'pipe ' + addr
+			: 'port ' + addr.port
+	;
 	console.log('Listening on ' + bind);
-	});
-	server.listen(port);
+});
+
+server.listen(port);
