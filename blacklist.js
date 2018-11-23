7daad0ca08029c5b8dd5088f947d8851e19082a3
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2018-11-22 19:37:42 -0800

    blacklist

--- blacklist.js
+++ blacklist.js
@@ -1,23 +1,23 @@
 // add items
-$('#add-todo').click(function(){
-  var lastSibling = $('#todo-list > .todo-wrap:last-of-type > input').attr('id');
+$('#add-blacklist').click(function(){
+  var lastSibling = $('#blacklist-list > .blacklist-wrap:last-of-type > input').attr('id');
   var newId = Number(lastSibling) + 1;
 
-  $(this).before('<span class="editing todo-wrap"><input type="checkbox" id="'+newId+'"/><label for="'+newId+'" class="todo"><i class="fa fa-check"></i><input type="text" class="input-todo" id="input-todo'+newId+'"/></label></div>');
-  $('#input-todo'+newId+'').parent().parent().animate({
+  $(this).before('<span class="editing blacklist-wrap"><input type="checkbox" id="'+newId+'"/><label for="'+newId+'" class="blacklist"><i class="fa fa-check"></i><input type="text" class="input-blacklist" id="input-blacklist'+newId+'"/></label></div>');
+  $('#input-blacklist'+newId+'').parent().parent().animate({
     height:"36px"
   },200)
-  $('#input-todo'+newId+'').focus();
+  $('#input-blacklist'+newId+'').focus();
 
-  $('#input-todo'+newId+'').enterKey(function(){
+  $('#input-blacklist'+newId+'').enterKey(function(){
     $(this).trigger('enterEvent');
   })
 
-  $('#input-todo'+newId+'').on('blur enterEvent',function(){
-    var todoTitle = $('#input-todo'+newId+'').val();
-    var todoTitleLength = todoTitle.length;
-    if (todoTitleLength > 0) {
-      $(this).before(todoTitle);
+  $('#input-blacklist'+newId+'').on('blur enterEvent',function(){
+    var blacklistTitle = $('#input-blacklist'+newId+'').val();
+    var blacklistTitleLength = blacklistTitle.length;
+    if (blacklistTitleLength > 0) {
+      $(this).before(blacklistTitle);
       $(this).parent().parent().removeClass('editing');
       $(this).parent().after('<span class="delete-item" title="remove"><i class="fa fa-times-circle"></i></span>');
       $(this).remove();
