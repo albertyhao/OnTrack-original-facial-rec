cf8e2bfaafe6e614b4b73ec96e5e7a10c1001328
Grant Cheng <chengxgrant@gmail.com>
2018-11-22 17:37:43 -0800

    Add paths

--- index.js
+++ index.js
@@ -15,11 +15,28 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/home', (req, res, next) => {
+app.get('/', (req, res, next) => {
 	console.log('home')
 	var filePath = path.join(__dirname, './home.html')
 	res.sendFile(filePath);
 });
+
+app.get('/blacklist', (req, res, next) => {
+	console.log('blacklist')
+	var filePath = path.join(__dirname, './blacklist.html')
+	res.sendFile(filePath);
+})
+
+app.get('/settings', (req, res, next) => {
+	console.log('settings')
+	var filePath = path.join(__dirname, './settings.html')
+	res.sendFile(filePath);
+})
+
+app.get('/sidebar.css', (req, res, next) => {
+	var filePath = path.join(__dirname, './sidebar.css')
+	res.sendFile(filePath);
+})
 
 server.on('listening', () => {
 	var addr = server.address()
