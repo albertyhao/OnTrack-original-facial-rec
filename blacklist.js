421ef09706d92849d09e8d126880e93a05efc63c
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2018-11-22 21:47:59 -0800

    blacklist

--- blacklist.js
+++ blacklist.js
@@ -1,69 +1,15 @@
-// add items
-$('#add-blacklist').click(function(){
-  var lastSibling = $('#blacklist-list > .blacklist-wrap:last-of-type > input').attr('id');
-  var newId = Number(lastSibling) + 1;
+function addNewBlacklist() {
+  var newBlacklistItem = document.getElementById("askForBlacklist").value;
+  var deleteIcon = document.querySelector("i");
+  var td1 = document.createElement("TH");
+  var td2 = document.createElement("TH");
+  var newRow = document.createElement("TR");
+  newRow.appendChild(td1);
+  newRow.appendChild(td2);
+  td1.innerHTML = `${newBlacklistItem}`;
+  td2.innerHTML = "<i class='far fa-times-circle'></i>";
+  document.querySelector("tbody").appendChild(newRow);
 
-  $(this).before('<span class="editing blacklist-wrap"><input type="checkbox" id="'+newId+'"/><label for="'+newId+'" class="blacklist"><i class="fa fa-check"></i><input type="text" class="input-blacklist" id="input-blacklist'+newId+'"/></label></div>');
-  $('#input-blacklist'+newId+'').parent().parent().animate({
-    height:"36px"
-  },200)
-  $('#input-blacklist'+newId+'').focus();
-
-  $('#input-blacklist'+newId+'').enterKey(function(){
-    $(this).trigger('enterEvent');
-  })
-
-  $('#input-blacklist'+newId+'').on('blur enterEvent',function(){
-    var blacklistTitle = $('#input-blacklist'+newId+'').val();
-    var blacklistTitleLength = blacklistTitle.length;
-    if (blacklistTitleLength > 0) {
-      $(this).before(blacklistTitle);
-      $(this).parent().parent().removeClass('editing');
-      $(this).parent().after('<span class="delete-item" title="remove"><i class="fa fa-times-circle"></i></span>');
-      $(this).remove();
-      $('.delete-item').click(function(){
-        var parentItem = $(this).parent();
-        parentItem.animate({
-          left:"-30%",
-          height:0,
-          opacity:0
-        },200);
-        setTimeout(function(){ $(parentItem).remove(); }, 1000);
-      });
-    }
-    else {
-      $('.editing').animate({
-        height:'0px'
-      },200);
-      setTimeout(function(){
-        $('.editing').remove()
-      },400)
-    }
-  })
-
-});
-
-// remove items
-
-$('.delete-item').click(function(){
-  var parentItem = $(this).parent();
-  parentItem.animate({
-    left:"-30%",
-    height:0,
-    opacity:0
-  },200);
-  setTimeout(function(){ $(parentItem).remove(); }, 1000);
-});
-
-// Enter Key detect
-
-$.fn.enterKey = function (fnc) {
-    return this.each(function () {
-        $(this).keypress(function (ev) {
-            var keycode = (ev.keyCode ? ev.keyCode : ev.which);
-            if (keycode == '13') {
-                fnc.call(this, ev);
-            }
-        })
-    })
 }
+
+document.getElementById("addToBlacklist").addEventListener("click", addNewBlacklist);
