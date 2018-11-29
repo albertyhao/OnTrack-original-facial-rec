db0ef282409c07bdd4a3720f7556f6d302499480
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2018-11-28 21:46:52 -0800

    bv

--- PackTrack/injectBackground.js
+++ PackTrack/injectBackground.js
@@ -7,4 +7,4 @@ chrome.webNavigation.onCompleted.addListener(function() {
   };
   (document.head || document.documentElement).appendChild(s);
 
- }, {url: [{urlMatches : 'https://www.hulu.com'}]});
+}, {url: [{urlMatches : 'https://www.hulu.com/*'}]});
