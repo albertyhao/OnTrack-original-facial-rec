bf831761694f8d770e0d633950092f9feca4f048
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2018-11-28 18:54:45 -0800

    Update background.js

--- PackTrack/background.js
+++ PackTrack/background.js
@@ -10,3 +10,7 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.bookmarks.onCreated.addListener(function() {
   // do something
 });
+
+chrome.webNavigation.onCompleted.addListener(function() {
+     alert("I love hulu!");
+ }, {url: [{urlMatches : 'https://www.hulu.com/'}]});
