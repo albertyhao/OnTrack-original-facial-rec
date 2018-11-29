b1b9b70396c0bc0b444463c2326dcac9d7c3d421
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2018-11-28 21:27:14 -0800

    Update background.js

--- PackTrack/background.js
+++ PackTrack/background.js
@@ -3,7 +3,15 @@
 //  }, {url: [{urlMatches : 'https://www.hulu.com'}]});
 // use for the blacklist
 
-chrome.webNavigation.onCompleted.addListener(function() {
-  var url = window.location.href;
-     alert(url);
- }, {url: [{urlMatches : 'https://www.hulu.com'}]});
+ var s = document.createElement('script');
+ // TODO: add "script.js" to web_accessible_resources in manifest.json
+ s.src = chrome.extension.getURL('background.js');
+ s.onload = function() {
+     this.remove();
+ };
+ (document.head || document.documentElement).appendChild(s);
+
+ chrome.webNavigation.onCompleted.addListener(function() {
+   var url = window.location.href;
+      alert(url);
+  }, {url: [{urlMatches : 'https://www.hulu.com'}]});
