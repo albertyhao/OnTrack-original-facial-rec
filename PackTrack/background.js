22c8f36f1671abe29f246213232c69f30e923ef0
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2018-11-28 21:52:12 -0800

    b

--- PackTrack/background.js
+++
@@ -1,7 +0,0 @@
-// chrome.webNavigation.onCompleted.addListener(function() {
-//      alert("I love hulu!");
-//  }, {url: [{urlMatches : 'https://www.hulu.com'}]});
-// use for the blacklist
-
-
-alert(window.location.href);
