0eff49ae74f04cde128fa143a23ac8ae442597bb
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2018-11-28 21:43:20 -0800

    ljb

--- PackTrack/background.js
+++ PackTrack/background.js
@@ -3,13 +3,5 @@
 //  }, {url: [{urlMatches : 'https://www.hulu.com'}]});
 // use for the blacklist
 
- chrome.webNavigation.onCompleted.addListener(function() {
-   var s = document.createElement('script');
-   // TODO: add "script.js" to web_accessible_resources in manifest.json
-   s.src = chrome.extension.getURL('injectBackground.js');
-   s.onload = function() {
-       this.remove();
-   };
-   (document.head || document.documentElement).appendChild(s);
 
-  }, {url: [{urlMatches : 'https://www.hulu.com'}]});
+alert(window.location.href);
