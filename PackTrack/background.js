490e3d988b0bda6ad78d0cd73d0d2f137c2bfe5d
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2018-11-28 18:53:14 -0800

    Create background.js

---
+++ PackTrack/background.js
@@ -0,0 +1,12 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    "id": "sampleContextMenu",
+    "title": "Sample Context Menu",
+    "contexts": ["selection"]
+  });
+});
+
+// This will run when a bookmark is created.
+chrome.bookmarks.onCreated.addListener(function() {
+  // do something
+});
