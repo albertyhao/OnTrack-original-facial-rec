8ba1077b1f73980f9c071a4b0b996b054cf24ced
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2018-11-28 19:39:09 -0800

    Update background.js

--- PackTrack/background.js
+++ PackTrack/background.js
@@ -1,16 +1,4 @@
-// chrome.runtime.onInstalled.addListener(function() {
-//   chrome.contextMenus.create({
-//     "id": "sampleContextMenu",
-//     "title": "Sample Context Menu",
-//     "contexts": ["selection"]
-//   });
-// });
-
-// This will run when a bookmark is created.
-// chrome.bookmarks.onCreated.addListener(function() {
-  // do something
-// });
-
-chrome.webNavigation.onCompleted.addListener(function() {
-     alert("I love hulu!");
- }, {url: [{urlMatches : 'https://www.hulu.com'}]});
+// chrome.webNavigation.onCompleted.addListener(function() {
+//      alert("I love hulu!");
+//  }, {url: [{urlMatches : 'https://www.hulu.com'}]});
+// use for the blacklist
