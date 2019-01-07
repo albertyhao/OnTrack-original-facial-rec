509ae9bfe227848d9f2fb0866150ffa9b062a6d4
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2019-01-07 15:20:23 -0800

    dsf

--- server.js
+++ server.js
@@ -4,29 +4,48 @@ var express = require('express')
 	, http = require('http')
 	, async = require('async')
 	, fs = require('fs')
+  , mongoose = require('mongoose')
+  , packtrackSchema = require("./packtrackSchema.js").getModel()
 ;
 
 var app = express()
 	, port = parseInt(process.env.PORT || '8080')
 	, server = http.createServer(app)
+  ,dbUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1/ontrack'
 ;
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect(dbUri, function(err){
+  if(err){
+    return console.log(err);
+  }
+  console.log('Connected to database');
+  var server = http.createServer(app)
+  app.set('trust proxy',true );
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res, next) => {
-	var filePath = path.join(__dirname, './.html')
-	res.sendFile(filePath);
+  app.get('/', (req, res, next) => {
+  	var filePath = path.join(__dirname, '/1px.png')
+  	res.sendFile(filePath);
+    var packtrack = new packtrackSchema({
+      url: req.query.url,
+      username: req.hostname,
+      image: undefined,
+      timestamp: new Date(),
+      ip: req.ip
+    })
+    packtrack.save()
+  })
+  server.on('listening', () => {
+  	var addr = server.address()
+  		, bind = typeof addr === 'string'
+  			? 'pipe ' + addr
+  			: 'port ' + addr.port
+  	;
+  	console.log('Listening on ' + bind);
+  });
+
+  server.listen(port);
+
 })
-
-server.on('listening', () => {
-	var addr = server.address()
-		, bind = typeof addr === 'string'
-			? 'pipe ' + addr
-			: 'port ' + addr.port
-	;
-	console.log('Listening on ' + bind);
-});
-
-server.listen(port);
