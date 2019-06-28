608c596167d70c411226d0b718b3af5c3b845aff
Nathan Lee <nathanlee@Nathans-MacBook-Pro.local>
2019-06-27 20:29:54 -0700

    dddd

--- blacklist.js
+++ blacklist.js
@@ -5,6 +5,7 @@ var blacklistItemNumber = 2; // sets the unique numbered id for each list item
 function addNewBlacklist() {
   var req = new XMLHttpRequest();
   req.open('POST', '/blacklist1', true);
+  console.log("1");
   req.setRequestHeader('content-type', 'application/json');
   req.onreadystatechange = function(){
     if(req.readyState != 4){
